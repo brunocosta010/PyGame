fdf34242857025cc0167ddf1eb0328fe1da058dd
Bruno Costa <bruno.mth.costa@gmail.com>
2025-07-29 20:43:33 -0300

    Creating enemy attack and Player Attack

--- code/Const.py
+++ code/Const.py
@@ -12,9 +12,13 @@ ENTITY_SPEED = {
     'Level1Bg1': 1,
     'Level1Bg2': 2,
     'Player1': 3,
+    'Player1Shot': 1,
     'Player2': 3,
-    'Enemy1': 2,
+    'Player2Shot': 1,
+    'Enemy1': 1,
+    'Enemy1Shot' : 5,
     'Enemy2': 1,
+    'Enemy2Shot': 2
 }
 
 # H
@@ -23,9 +27,21 @@ ENTITY_HEALTH = {
     'Level1Bg1': 999,
     'Level1Bg2': 999,
     'Player1': 300,
+    'Player1Shot': 1,
     'Player2': 300,
+    'Player2Shot': 1,
     'Enemy1': 50,
+    'Enemy1Shot': 1,
     'Enemy2': 60,
+    'Enemy2Shot': 1,
+}
+
+# E
+ENTITY_SHOT_DELAY = {
+    'Player1' : 20,
+    'Player2' : 15,
+    'Enemy1': 100,
+    'Enemy2': 200
 }
 
 # M
@@ -59,8 +75,8 @@ PLAYER_KEY_RIGHT = {
 }
 
 PLAYER_KEY_SHOOT = {
-    'Player1': pygame.K_RCTRL,
-    'Player2': pygame.K_LCTRL
+    'Player1': pygame.K_LCTRL,
+    'Player2': pygame.K_RCTRL
 }
 
 # S
