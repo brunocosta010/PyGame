5a8a5cae69854c806209a89d35e5d68e7243dfa1
Bruno Costa <bruno.mth.costa@gmail.com>
2025-07-29 11:48:37 -0300

    Creating players and enemies Moving players and enemies.

--- code/background.py
+++ code/background.py
@@ -9,6 +9,6 @@ class Background(Entity):
         super().__init__(name, position)
 
     def move(self, ):
-        self.rect.centerx -= ENTITY_SPEED [self.name] # Velocidade (Deixar assim para ficar estático self.rect.centerx -= 1)
+        self.rect.centerx -= ENTITY_SPEED [self.name] # Velocidade (Deixar assim para ficar estático: self.rect.centerx -= 1)
         if self.rect.right <= 0:
             self.rect.left = WIN_WIDTH
